7da743c473e55fed174e19c377a0abd104454c63
Kriyo <krikenn@gmail.com>
2020-05-25 14:27:17 +0100

    updated imports & exports

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,31 +1,29 @@
 import styled from "styled-components";
-
-const primaryColor = "#2E4052";
-const secondaryColor = "#FFF";
+import { defaultTheme, typeScale } from "../utils";
 
 const Button = styled.button`
   padding: 12px 24px;
-  font-size: 1rem;
+  font-size: ${typeScale.paragraph};
   min-width: 100px;
   cursor: pointer;
   font-family: "Montserrat", sans-serif;
 `;
 
 export const PrimaryButton = styled(Button)`
-  background-color: ${primaryColor};
+  background-color: ${defaultTheme.primaryColor};
   border: none;
   color: white;
 `;
 
 export const SecondaryButton = styled(Button)`
-  background-color: ${secondaryColor};
-  border: 1px solid ${primaryColor};
-  color: ${primaryColor};
+  background-color: ${defaultTheme.textColorOnPrimary};
+  border: 1px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
 `;
 export const TertiaryButton = styled(Button)`
-  background-color: ${secondaryColor};
+  background-color: ${defaultTheme.textColorOnPrimary};
   border: none;
-  color: ${primaryColor};
+  color: ${defaultTheme.primaryColor};
 `;
 
 export default PrimaryButton;
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,4 +1,4 @@
-const primary = {
+export const primary = {
   100: "#2E4052",
   200: "#FFFC31",
   300: "#6B6D76",
@@ -6,7 +6,7 @@ const primary = {
   500: "#404E4D",
 };
 
-const neutrals = {
+export const neutral = {
   100: "#FFF",
   200: "#F4F5F7",
   300: "#E1E1E1",
@@ -15,19 +15,19 @@ const neutrals = {
   600: "#000",
 };
 
-const success = {
+export const success = {
   100: "#529E66",
   200: "#367B48",
   300: "#276738",
 };
 
-const error = {
+export const error = {
   100: "#D0454C",
   200: "#B54248",
   300: "#95353A",
 };
 
-const warning = {
+export const warning = {
   100: "#E1C542",
   200: "#CAB23F",
   300: "#B49E35",
