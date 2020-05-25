066de7782559394724ad27c330d01c9231fcf724
Kriyo <krikenn@gmail.com>
2020-05-25 13:45:14 +0100

    added cdn link for font and put a generic button in place

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 
-const PrimaryButton = styled.button`
-  background-color: red;
-  border: none;
-  color: white;
+const primaryColor = "#2E4052";
+
+const Button = styled.button`
   padding: 12px 24px;
   font-size: 1rem;
+  min-width: 100px;
+  cursor: pointer;
+  font-family: "Montserrat", sans-serif;
+`;
+
+const PrimaryButton = styled(Button)`
+  background-color: ${primaryColor};
+  border: none;
+  color: white;
 `;
 
 export default PrimaryButton;
